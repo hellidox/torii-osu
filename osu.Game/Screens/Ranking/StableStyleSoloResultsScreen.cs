b9970c086ec89f9627cc5cc253b9bb4380d00ee9
Shikkesora <shikkesora@gmail.com>
2026-03-18 01:18:22 -0300

    Improve stable-style song select/results typography

--- osu.Game/Screens/Ranking/StableStyleSoloResultsScreen.cs
+++ osu.Game/Screens/Ranking/StableStyleSoloResultsScreen.cs
@@ -1,6 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using osu.Framework.Graphics;
+using osu.Framework.Testing;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
 using osu.Game.Scoring;
 
 namespace osu.Game.Screens.Ranking
@@ -12,9 +17,30 @@ namespace osu.Game.Screens.Ranking
     /// </summary>
     public partial class StableStyleSoloResultsScreen : SoloResultsScreen
     {
+        private readonly HashSet<Drawable> styledTextCache = new HashSet<Drawable>();
+        private double lastStylePassTime;
+
         public StableStyleSoloResultsScreen(ScoreInfo score)
             : base(score)
         {
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if (Time.Current - lastStylePassTime < 250)
+                return;
+
+            lastStylePassTime = Time.Current;
+
+            foreach (var text in this.ChildrenOfType<OsuSpriteText>())
+            {
+                if (!styledTextCache.Add(text))
+                    continue;
+
+                text.Font = text.Font.With(typeface: Typeface.Venera);
+            }
         }
     }
 }
--- osu.Game/Screens/Select/StableStyleSoloSongSelect.cs
+++ osu.Game/Screens/Select/StableStyleSoloSongSelect.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
 using osu.Framework.Audio.Sample;
+using osu.Framework.Graphics;
 using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Input;
 using osu.Framework.Screens;
+using osu.Framework.Testing;
 using osu.Game.Beatmaps;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Localisation;
 using osu.Game.Overlays;
@@ -33,6 +37,9 @@ namespace osu.Game.Screens.Select
     {
         protected override UserActivity InitialActivity => new UserActivity.ChoosingBeatmap();
 
+        private readonly HashSet<Drawable> styledTextCache = new HashSet<Drawable>();
+        private double lastStylePassTime;
+
         [Resolved]
         private INotificationOverlay? notifications { get; set; }
 
@@ -48,7 +55,7 @@ namespace osu.Game.Screens.Select
             AddInternal(new SongSelectTouchInputDetector());
         }
 
-        protected override BeatmapDetailArea CreateBeatmapDetailArea() => new PlayBeatmapDetailArea();
+        protected override BeatmapDetailArea CreateBeatmapDetailArea() => new StableStylePlayBeatmapDetailArea();
 
         public override MenuItem[] CreateForwardNavigationMenuItemsForBeatmap(Func<BeatmapInfo> getBeatmap) => new MenuItem[]
         {
@@ -127,6 +134,24 @@ namespace osu.Game.Screens.Select
             playerLoader = null;
         }
 
+        protected override void Update()
+        {
+            base.Update();
+
+            if (Time.Current - lastStylePassTime < 250)
+                return;
+
+            lastStylePassTime = Time.Current;
+
+            foreach (var text in this.ChildrenOfType<OsuSpriteText>())
+            {
+                if (!styledTextCache.Add(text))
+                    continue;
+
+                text.Font = text.Font.With(typeface: Typeface.Venera);
+            }
+        }
+
         private partial class PlayerLoader : Play.PlayerLoader
         {
             public override bool ShowFooter => !QuickRestart;
@@ -134,6 +159,16 @@ namespace osu.Game.Screens.Select
             public PlayerLoader(Func<Player> createPlayer)
                 : base(createPlayer)
             {
+            }
+        }
+
+        private partial class StableStylePlayBeatmapDetailArea : PlayBeatmapDetailArea
+        {
+            [BackgroundDependencyLoader]
+            private void load()
+            {
+                foreach (var text in this.ChildrenOfType<OsuSpriteText>())
+                    text.Font = text.Font.With(typeface: Typeface.Venera);
             }
         }
     }
