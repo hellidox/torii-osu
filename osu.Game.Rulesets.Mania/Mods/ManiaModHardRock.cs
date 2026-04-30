0608a7f8fb2a4345b1fb30d410c834fe354f81b2
ana <ss@g>
2026-04-30 16:40:13 -0500

    rank mania hard rock

--- osu.Game.Rulesets.Mania/Mods/ManiaModHardRock.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModHardRock.cs
@@ -11,7 +11,6 @@ namespace osu.Game.Rulesets.Mania.Mods
     public class ManiaModHardRock : ModHardRock, IApplicableToHitObject
     {
         public override double ScoreMultiplier => 1;
-        public override bool Ranked => false;
 
         public const double HIT_WINDOW_DIFFICULTY_MULTIPLIER = 1.4;
 
