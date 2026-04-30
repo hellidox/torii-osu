55e3aa87163752d061658db182620fe782334dc0
andrea <whenthe.376@gmail.com>
2026-04-30 12:18:35 -0500

    unrank adaptive speed

--- osu.Game/Rulesets/Mods/ModAdaptiveSpeed.cs
+++ osu.Game/Rulesets/Mods/ModAdaptiveSpeed.cs
@@ -34,6 +34,7 @@ namespace osu.Game.Rulesets.Mods
         public override ModType Type => ModType.Fun;
 
         public override double ScoreMultiplier => 0.5;
+        public override bool Ranked => false;
 
         public sealed override bool ValidForMultiplayer => false;
         public sealed override bool ValidForMultiplayerAsFreeMod => false;
