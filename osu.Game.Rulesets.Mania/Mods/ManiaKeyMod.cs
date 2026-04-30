2a78ded6b4baf4552d931dbae08922c5abfc382f
ana <ss@g>
2026-04-30 16:41:52 -0500

    remove redundant UsesDefaultConfiguration in mod ranked

--- osu.Game.Rulesets.Mania/Mods/ManiaKeyMod.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaKeyMod.cs
@@ -15,7 +15,7 @@ namespace osu.Game.Rulesets.Mania.Mods
         public abstract int KeyCount { get; }
         public override ModType Type => ModType.Conversion;
         public override double ScoreMultiplier => 0.9;
-        public override bool Ranked => UsesDefaultConfiguration;
+
 
         public void ApplyToBeatmapConverter(IBeatmapConverter beatmapConverter)
         {
