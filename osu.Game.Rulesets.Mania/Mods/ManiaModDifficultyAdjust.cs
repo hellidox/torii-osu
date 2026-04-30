e987d28a4e3b583b96b79f653b2f23f32c0713df
andrea <whenthe.376@gmail.com>
2026-04-30 12:23:12 -0500

    add mania da od criteria (needs testing)

--- osu.Game.Rulesets.Mania/Mods/ManiaModDifficultyAdjust.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModDifficultyAdjust.cs
@@ -17,5 +17,6 @@ namespace osu.Game.Rulesets.Mania.Mods
             ExtendedMinValue = -15,
             ReadCurrentFromDifficulty = diff => diff.OverallDifficulty,
         };
+        public override bool Ranked => (OverallDifficulty.Value ?? 8f) >= 6f
     }
 }
